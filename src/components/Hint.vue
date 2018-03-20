ced961b2b25b78721672a85503d60b19292037e0
kone <kone@hudongpai.com>
2018-03-20 18:40:25 +0800

    001

---
+++ src/components/Hint.vue
@@ -0,0 +1,19 @@
+<template>
+    <div style="width:50px; height:50px; background: yellow;">
+        hint
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Hint',
+    data () {
+        return {
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
+
---
+++ src/components/Home.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="page-home">
+    {{msg}}
+    <button v-on:click="reverseMsg">逆转消息</button>
+    <Hint></Hint>
+    <runoob></runoob>
+</div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Hint from '@/components/Hint'
+// 注册全局组件。 任何地方都能使用  <Hint>
+// Vue.component('Hint', Hint)
+
+export default {
+    name: 'Home',
+    data () {
+        return {
+            msg: 'hello kaye!!!'
+        }
+    },
+    methods: {
+        reverseMsg: function () {
+            this.msg = this.msg.split('').reverse().join('')
+        }
+    },
+    components: {
+        //注册局部组件。 <Hint> 将只在父模板可用
+        'Hint': Hint
+    }
+}
+</script>
+
+
+
+<style scoped lang="less" rel="stylesheet/less">
+@clr:green;
+.page-home {
+    width:100px;
+    height: 100px;
+    background: red;
+    background: @clr;
+}
+</style>
